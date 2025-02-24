fbb632ae1406b993a7867fc1d7a5dd7111a060f5
navas <muhammednavas320203@gmail.com>
2025-02-25 00:26:38 +0530

    fix: chat input config changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import OllamaChat from "@/components/page/ollamaChat";
-import { Input } from "@/components/ui/input";
 import {
   Select,
   SelectContent,
@@ -13,7 +12,7 @@ import { ChatModel } from "@/lib/model/chatModel";
 import axios from "axios";
 import { EarthIcon, MoonIcon, PaperclipIcon, SunIcon } from "lucide-react";
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { toast } from "sonner";
 
 export default function Home() {
@@ -24,6 +23,7 @@ export default function Home() {
   const [chat, setChat] = useState<ChatModel[]>([]);
 
   const inputContainerRef = useRef<HTMLFormElement>(null);
+  const placeHolderRef = useRef<HTMLParagraphElement>(null);
 
   useEffect(() => {
     const localTheme = localStorage.getItem("theme") || "light";
@@ -36,8 +36,22 @@ export default function Home() {
     document.documentElement.classList.toggle("dark", theme === "dark");
   }, [theme]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
+  const handleInputChange = (e: React.FormEvent<HTMLDivElement>) => {
+    const textContent = e.currentTarget.textContent || "";
+    setInput(textContent);
+    console.log(textContent.length, placeHolderRef);
+
+    if(placeHolderRef.current && textContent.length <= 0){
+      placeHolderRef.current.setAttribute("data-placeholder", "Ask anything");
+      placeHolderRef.current.textContent = "";
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleSubmit(e as unknown as React.FormEvent<HTMLFormElement>);
+    }
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -46,13 +60,19 @@ export default function Home() {
 
     setIsSubmitted(true);
     setInput("");
+
+    if(placeHolderRef.current){
+      placeHolderRef.current.setAttribute("data-placeholder", "Ask anything...");
+      placeHolderRef.current.textContent = "";
+    }
+
     setChat((prev) => [
       ...prev,
       {
         id: prev.length + 1,
         name: input,
         role: "user",
-        isError: false
+        isError: false,
       },
     ]);
 
@@ -63,7 +83,7 @@ export default function Home() {
   const ollamaChatCompletion = async () => {
     try {
       const response = await axios.post(`/api/ollama`, {
-        model: "llama3",
+        model: "llama3.2:latest",
         prompt: input,
         stream: false,
       });
@@ -74,23 +94,23 @@ export default function Home() {
           id: prev.length + 1,
           name: response.data.data.response || "",
           role: "Assistant",
-          isError: false
+          isError: false,
         },
       ]);
 
       setIsLoading(false);
     } catch (error: any) {
       setIsLoading(false);
-      const {response} = error
+      const { response } = error;
 
-      let message = ""
+      let message = "";
 
-      if(!response){
-        toast.error('Something went wrong');
-        message = "Something went wrong,"
+      if (!response) {
+        toast.error("Something went wrong");
+        message = "Something went wrong,";
       }
       toast.error(response.data?.error || "Something went wrong");
-      message = (response.data?.error || "Something went wrong,")
+      message = response.data?.error || "Something went wrong,";
 
       setChat((prev) => [
         ...prev,
@@ -98,12 +118,10 @@ export default function Home() {
           id: prev.length + 1,
           name: message,
           role: "Assistant",
-          isError: true
+          isError: true,
         },
       ]);
     }
-
-
   };
 
   return (
@@ -160,12 +178,24 @@ export default function Home() {
             isSubmitted ? "translate-y-72" : "translate-y-5"
           } flex flex-col border border-gray-300 dark:border-gray-900 shadow-lg rounded-lg bg-background mt-8 max-w-screen-md w-full`}
         >
-          <Input
-            value={input}
-            onChange={handleInputChange}
-            placeholder="Ask a question"
-            className="border-none focus-visible:ring-0 shadow-none bg-transparent text-gray-900 dark:text-gray-100 break-words"
-          />
+          <div
+            contentEditable={true}
+            translate="no"
+            onInput={handleInputChange}
+            onKeyDown={handleKeyDown}
+            suppressContentEditableWarning={true}
+            className="group ProseMirror border-none focus-visible:ring-0 focus:outline-none break-words overflow-auto min-h-[44px] max-h-32"
+            id="prompt-textarea"
+            data-virtualkeyboard="true"
+          >
+            <p
+             ref={placeHolderRef}
+              className="whitespace-pre-wrap text-gray-500 empty:before:content-[attr(data-placeholder)] dark:text-gray-400 placeholder"
+              data-placeholder="Ask a question"
+            >
+            </p>
+          </div>
+
           <div className="flex justify-between items-center mt-3 px-2">
             <div className="flex items-center gap-3">
               <div className="relative p-2 rounded-lg cursor-pointer bg-gray-200 dark:bg-background-secondary">
